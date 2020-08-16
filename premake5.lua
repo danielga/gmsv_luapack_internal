31178516c3a9b6901805447c921794aedc9648e7
Daniel <danielga@outlook.pt>
2020-08-16 17:27:56 +0100

    Update symbols (using garrysmod_common helpers)
    Add Azure Pipelines config
    Update license copyright years
    Update project generator version

--- premake5.lua
+++ premake5.lua
@@ -1,16 +1,19 @@
+PROJECT_GENERATOR_VERSION = 2
+
 newoption({
 	trigger = "gmcommon",
 	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
 	value = "path to garrysmod_common directory"
 })
 
-local gmcommon = _OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON")
-assert(gmcommon ~= nil, "you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
+local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
+	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(gmcommon)
 
 CreateWorkspace({name = "luapack_internal", abi_compatible = true})
 	CreateProject({serverside = true})
 		IncludeLuaShared()
+		IncludeHelpersExtended()
 		IncludeSDKCommon()
 		IncludeSDKTier0()
 		IncludeSDKTier1()
